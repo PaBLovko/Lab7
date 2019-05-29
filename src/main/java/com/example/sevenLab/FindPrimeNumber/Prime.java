21038871fde089d8a2ca4e9ce8dfd11c78ca42bc
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 18:12:44 +0300

    Delete Prime.java

--- src/main/java/com/example/sevenLab/FindPrimeNumber/Prime.java
+++
@@ -1,9 +0,0 @@
-package com.example.sevenLab.FindPrimeNumber;
-
-import com.example.sevenLab.MyException.MyException;
-
-import java.util.List;
-
-public interface Prime {
-    List<Integer> findPrimeNumbers(List<Integer> list) throws MyException;
-}
