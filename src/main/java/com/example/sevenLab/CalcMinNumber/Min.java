990131af4c4d294460be66599fa5b68acb4d3199
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 18:12:14 +0300

    Delete Min.java

--- src/main/java/com/example/sevenLab/CalcMinNumber/Min.java
+++
@@ -1,9 +0,0 @@
-package com.example.sevenLab.CalcMinNumber;
-
-import com.example.sevenLab.MyException.MyException;
-
-import java.util.List;
-
-public interface Min {
-    int minElem(List<Integer> list) throws MyException;
-}
